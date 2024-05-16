83bef3a03d4b1165bb937fb5808e579f86943c80
unknown <arthurzhuravetskiy@gmail.com>
2024-05-17 00:25:28 +0200

    Adding Categories

--- src/Item.jsx
+++ src/Item.jsx
@@ -15,6 +15,11 @@ const ItemStyles = styled.div`
     text-decoration: none;
     cursor: pointer;
   }
+  .items-ctnr {
+    display: flex;
+    flex-direction: column;
+    background-color: white;
+  }
 
   a:link {
     text-decoration: none;
@@ -94,6 +99,64 @@ const ItemStyles = styled.div`
     cursor: pointer;
   }
 
+  .lower-hr {
+    width: 100%;
+    height: 3px;
+    background-color: gray;
+  }
+
+  .categories-ctnr {
+    font-size: 1.3rem;
+    font-weight: 600;
+    width: 90vw;
+    font-family: "URW DIN SemiCond", sans-serif;
+  }
+
+  /* .categories-pstn {
+    
+  }
+
+ 
+  } */
+  @keyframes categorieslide {
+    100% {
+      z-index: 1;
+      height: 220px;
+      overflow: hidden;
+    }
+  }
+
+  .categories-mdl {
+    position: absolute;
+    top: 567px;
+    right: 0px;
+    z-index: 0;
+    overflow: hidden;
+    animation: categorieslide 1s forwards;
+    height: 0px;
+    background-color: white;
+    padding: 0px 50px;
+    display: grid;
+    width: 100%;
+    grid-column-gap: 20px;
+    grid-template-columns: repeat(2, 1fr);
+    font-size: 0.9rem;
+    font-family: "URW DIN", sans-serif;
+    font-weight: 400;
+  }
+
+  #ctgr-click {
+    cursor: pointer;
+  }
+
+  .ctgr {
+    margin-top: 10px;
+    cursor: pointer;
+  }
+
+  .lower-brt {
+    filter: brightness(50%);
+  }
   @media (max-width: 1550px) {
     .grid-ctnr {
       grid-template-columns: repeat(3, 1fr);
@@ -171,6 +234,7 @@ const ItemStyles = styled.div`
 
 const Item = (props) => {
   const [data, setData] = useState();
+  const [showCategories, setShowCategories] = useState(false);
 
   const AddToCart = (product) => {
     if (props.isLogged()) {
@@ -209,12 +273,90 @@ const Item = (props) => {
       .post("https://clothingbrandbackend.onrender.com/getProducts")
       .then((products) => setData(products.data))
       .catch((err) => console.log(err));
+
+    if (showCategories) {
+      document.querySelector(".items-ctnr").classList.add("lower-brt");
+    }
   }, []);
+
+  const DisplayItems = (ctgr) => {
+    axios
+      .post("https://clothingbrandbackend.onrender.com/getProductsByTag", {
+        ctgr,
+      })
+      .then((product) => setData(product.data))
+      .catch((err) => console.log(err));
+  };
 
   return (
     <ItemStyles>
       <div>
         <div className="items-ctnr">
+          <div className="categories-ctnr">
+            <div
+              id="ctgr-click"
+              onClick={() => setShowCategories(!showCategories)}
+            >
+              CATEGORIES
+            </div>
+            <hr className="lower-hr" />
+            {showCategories ? (
+              <div className="categories-mdl">
+                <div className="clmn1">
+                  <div className="ctgr" onClick={DisplayItems("Packs")}>
+                    Packs
+                  </div>
+                  <div className="ctgr" onClick={DisplayItems("Accessories")}>
+                    Accessories
+                  </div>
+                  <div className="ctgr" onClick={DisplayItems("Shell Jackets")}>
+                    Shell Jackets
+                  </div>
+                  <div
+                    className="ctgr"
+                    onClick={DisplayItems("Insulated Jackets")}
+                  >
+                    Insulated Jackets
+                  </div>
+                  <div
+                    className="ctgr"
+                    onClick={DisplayItems("Shirts and Tops")}
+                  >
+                    Shirts and Tops
+                  </div>
+                  <div className="ctgr" onClick={DisplayItems("Pants")}>
+                    Pants
+                  </div>
+                  <div className="ctgr" onClick={DisplayItems("Fleece")}>
+                    Fleece
+                  </div>
+                </div>
+                <div className="clmn2">
+                  <div className="ctgr" onClick={DisplayItems("Footwear")}>
+                    Footwear
+                  </div>
+                  <div className="ctgr" onClick={DisplayItems("Climbing Gear")}>
+                    Climbing Gear
+                  </div>
+                  <div className="ctgr" onClick={DisplayItems("Base Layer")}>
+                    Base Layer
+                  </div>
+                  <div className="ctgr" onClick={DisplayItems("Shorts")}>
+                    Shorts
+                  </div>
+                  <div
+                    className="ctgr"
+                    onClick={DisplayItems("Apperal Accessories")}
+                  >
+                    Apperal Accessories
+                  </div>
+                </div>
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
+
           <div className="grid-ctnr">
             {data?.map((product, key) => (
               <Link to={"/itemPage"} key={product.id} className="item">
